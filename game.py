b9af028921b466bd868e2db8c200abf9a06cec08
a.pogorzelski <pogorzelski.adrian@gmail.com>
2023-07-30 08:36:39 +0200

    Game playable

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import random
-import math
+
 
 # PLAYER CLASS
 class Player():
@@ -25,21 +25,14 @@ class Player():
         print('*********')
         print("Playing as " + player.symbol)
 
+
 # BOARD CLASS
 class Board():
     def __init__(self):
         # CREATE BOARD
-        self.board = [1,2,3,4,5,6,7,8,9]
+        self.board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
         # Scoring threes
-        self.topRow = [self.board[0], self.board[1], self.board[2]]
-        self.middleRow = [self.board[3], self.board[4], self.board[5]]
-        self.bottomRow = [self.board[0], self.board[0], self.board[0]]
-        self.leftColumn = [self.board[0], self.board[0], self.board[0]]
-        self.centerColumn = [self.board[0], self.board[0], self.board[0]]
-        self.rightColumn = [self.board[0], self.board[0], self.board[0]]
-        self.topDiagonal = [self.board[0], self.board[0], self.board[0]]
-        self.bottomDiagonal = [self.board[0], self.board[0], self.board[0]]
         # Print board
 
     def drawBoard(self):
@@ -51,6 +44,25 @@ class Board():
         if int(input) in self.board:
             return True
 
+    def checkWin(self):
+        topRow = [self.board[0], self.board[1], self.board[2]]
+        middleRow = [self.board[3], self.board[4], self.board[5]]
+        bottomRow = [self.board[6], self.board[7], self.board[8]]
+        leftColumn = [self.board[0], self.board[3], self.board[6]]
+        centerColumn = [self.board[1], self.board[4], self.board[7]]
+        rightColumn = [self.board[2], self.board[5], self.board[8]]
+        topDiagonal = [self.board[0], self.board[4], self.board[8]]
+        bottomDiagonal = [self.board[6], self.board[4], self.board[2]]
+        if len(set(topRow)) == 1\
+                or len(set(middleRow)) == 1 \
+                or len(set(bottomRow)) == 1 \
+                or len(set(leftColumn)) == 1 \
+                or len(set(centerColumn)) == 1 \
+                or len(set(rightColumn)) == 1 \
+                or len(set(topDiagonal)) == 1 \
+                or len(set(bottomDiagonal)) == 1:
+            return True
+
     # Players move
     def playerMove(self):
         ### PLAYER MOVE:
@@ -59,47 +71,64 @@ class Board():
             self.board[int(playerInput) - 1] = player.symbol
         else:
             self.playerMove()
-        self.drawBoard()
-        self.cpuMove()
         #### DRAW MOVE ON BOARD
+        self.drawBoard()
         #### CHECK FOR WIN
+        if self.checkWin():
+            print(player.playerName + " wins!")
+            player.score += 1
+            game.anotherGame()
+        else:
+            self.cpuMove()
+
     ### CPU MOVE:
     def cpuMove(self):
-        print('CPU move')
         cpuInput = random.randint(0, 8)
         if self.checkMove(cpuInput):
+            print('CPU move')
             self.board[int(cpuInput) - 1] = cpu.symbol
         else:
             self.cpuMove()
+        #### DRAW MOVE ON BOARD
         self.drawBoard()
-        self.playerMove()
-    #### DRAW MOVE ON BOARD
         #### CHECK FOR WIN
-
-def startGame():
-    player.setPlayerSymbol()
-    board = Board()
-    board.drawBoard()
-    if player.symbol == 'O':
-        board.playerMove()
-    else:
-        board.cpuMove()
-
-    ## PRINT RESULT
-    print('Score:\n'+ player.playerName +': ' + str(player.score) + '\nCPU: ' + str(cpu.score))
-    ## ASK FOR ANOTHER GAME
-    def anotherGame():
-        another = input('Do you want to play another round? (Y/N): ')
-        if another.lower() == 'y':
-            startGame()
-        elif another.lower() == 'n':
-            print("Thank you for playing!")
+        if self.checkWin():
+            print("CPU wins!")
+            cpu.score += 1
+            game.anotherGame()
         else:
-            anotherGame()
-    anotherGame()
+            self.playerMove()
+        self.playerMove()
+
+class Game():
+
+    def startGame(self):
+        player.setPlayerSymbol()
+        board = Board()
+        board.drawBoard()
+        if player.symbol == 'O':
+            board.playerMove()
+        else:
+            board.cpuMove()
+
+    ## ASK FOR ANOTHER GAME
+    def anotherGame(self):
+        ## PRINT RESULT
+        print('Score:\n' + player.playerName + ': ' + str(player.score) + '\nCPU: ' + str(cpu.score))
+        self.another = input('Do you want to play another round? (Y/N): ')
+        if self.another.lower() == 'y':
+            self.startGame()
+        elif self.another.lower() == 'n':
+            print("Thank you for playing!")
+            exit()
+        else:
+            self.anotherGame()
+
+        self.anotherGame()
+
 
 # GAME
-if __name__=='__main__':
+if __name__ == '__main__':
     # WELCOME MESSAGE
     print('Tic-Tac-Toe')
     print('*********')
@@ -109,4 +138,5 @@ if __name__=='__main__':
     cpu = Player('CPU')
 
     # START GAME
-    startGame()
+    game = Game()
+    game.startGame()
